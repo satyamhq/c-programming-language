c2abf6328798f7fd4502737cc4edc37a9a7db071
Satyam Kumar <satyam31sk@gmail.com>
2026-01-28 10:08:43 +0530

    Create 04-reg-fee-program.c

---
+++ module1-basic-programs/04-reg-fee-program.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int reg = 12505700;
+    int exam_fee = 98000;
+
+    printf("Registration Number: %d\n", reg);
+    printf("Exam Fee: %d\n", exam_fee);
+
+    return 0;
+}
