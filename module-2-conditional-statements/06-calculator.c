5b6c5b7965d233b0f3bd37f37bb4210e8729c1d2
Satyam Kumar <satyam31sk@gmail.com>
2026-02-10 15:40:34 +0530

    create 06-calculator.c

---
+++ module-2-conditional-statements/06-calculator.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    char op;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);   // space before %c is important
+
+    switch (op) {
+        case '+':
+            printf("Result = %d", num1 + num2);
+            break;
+
+        case '-':
+            printf("Result = %d", num1 - num2);
+            break;
+
+        case '*':
+            printf("Result = %d", num1 * num2);
+            break;
+
+        case '/':
+            if (num2 != 0)
+                printf("Result = %d", num1 / num2);
+            else
+                printf("Division not possible");
+            break;
+
+        default:
+            printf("Invalid operator");
+            break;
+    }
+
+    return 0;
+}
