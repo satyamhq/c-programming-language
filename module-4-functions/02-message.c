0e7762f1ea2fbfb4b4a47601c87108c6d1ebb2b9
Satyam Kumar <satyam31sk@gmail.com>
2026-03-02 16:06:37 +0530

    create 02-message.c

---
+++ module-4-functions/02-message.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void message();
+
+void message() {
+    printf("We are learning C programming!\n");
+}
+
+int main() {
+    message();
+    return 0;
+}
---
+++ module-4-functions/03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void message();
+
+int rev (int n);
+
+void discount(int bill);
+
+void message() {
+    printf("We are learning C programming!\n");
+}
+int main() {
+    message();
+}
