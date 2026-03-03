72d235bd0b6dca140c1b15f6e6b4ac6bbf3f97bc
Satyam Kumar <satyam31sk@gmail.com>
2026-03-03 15:27:37 +0530

    update 06-cubic-sum.c

--- module-4-functions/06-cubic-sum.c
+++ module-4-functions/06-cubic-sum.c
@@ -1,14 +1,34 @@
 // Write a user define function to compute the cubic sum of its digit.
 #include <stdio.h>
+
+int cubic_sum(int n) {
+    int d, s = 0;
+
+    if (n < 0) {
+        return "Invalid";
+    }
+
+    while (n != 0) {
+        d = n % 10; 
+        s = s + (d * d * d);
+        n = n / 10;
+    }
+
+    return s;
+}
+
 int main() {
-    int num, sum = 0, rem;
+    int num, result;
+
     printf("Enter a number: ");
     scanf("%d", &num);
-    while(num > 0) {
-        rem = num % 10;
-        sum = sum + (rem * rem * rem);
-        num = num / 10;
-    }
-    printf("Cubic sum of digits is: %d", sum);
+
+    result = cubic_sum(num);
+
+    if (result == -1)
+        printf("Invalid number\n");
+    else
+        printf("Cubic sum of digits = %d\n", result);
+
     return 0;
 }
