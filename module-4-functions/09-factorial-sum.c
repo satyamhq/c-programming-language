ec5b269802b1827f741df0f98ab8be4d4f667b1c
Satyam Kumar <satyam31sk@gmail.com>
2026-03-09 15:49:53 +0530

    update 09-factorial-sum.c

--- module-4-functions/09-factorial-sum.c
+++ module-4-functions/09-factorial-sum.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
 
-int fact(int n) {
-    int i, f = 1;
-    for(i = 1; i <= n; i++) {
-        f = f * i;
+int fact_sum_digits (int n) {
+    int d, s = 0, org = n, fact = 1;
+    while  (n > 0) {
+        d = n % 10;
+        for (int i = 1; i <= d; i++) {
+            fact *= i;
+        }
+        s += fact;
+        fact = 1;
+        n /= 10;
     }
-    return f;
-}
-
-int factorial_sum(int num) {
-    int digit, sum = 0;
-
-    while(num != 0) {
-        digit = num % 10;        
-        sum = sum + fact(digit); 
-        num = num / 10;           
-    }
-
-    return sum;
-}
-
-int main() {
-    int result;
-    result = factorial_sum(123);
-    printf("Sum of factorial of digits = %d", result);
-    return 0;
+    printf("Sum of factorial of digits of %d is %d\n", org, s);
 }
---
+++ module-4-functions/10-strong-number.c
@@ -0,0 +1,30 @@
+// function to check strong number
+int check_strong_num(int n) {
+    int original = n;
+    int sum = 0, digit, i, fact;
+
+    while(n > 0) {
+        digit = n % 10;
+
+        fact = 1;
+        for(i = 1; i <= digit; i++) {
+            fact = fact * i;
+        }
+
+        sum = sum + fact;
+        n = n / 10;
+    }
+
+    if(sum == original)
+        printf("%d is a strong number.\n", original);
+    else
+        printf("%d is not a strong number.\n", original);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    check_strong_num(num);
+    return 0;
+}
