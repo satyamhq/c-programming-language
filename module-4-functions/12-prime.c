98e01aaeae9b9e96728335c28edff027a8b63d2c
Satyam Kumar <satyam31sk@gmail.com>
2026-03-10 15:07:44 +0530

    create

--- module-4-functions/12-prime.c
+++ module-4-functions/12-prime.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 
-void check_prime(int n) {
+int is_prime(int n) {
     int i;
 
-    if (n <= 1) {
-        printf("%d is not a prime number.\n", n);
-        return;
-    }
+    if (n <= 1)
+        return 0;
 
     for (i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            printf("%d is not a prime number.\n", n);
-            return;
-        }
+        if (n % i == 0)
+            return 0;
     }
 
-    printf("%d is a prime number.\n", n);
+    return 1;
 }
 
 int main() {
     int num;
     printf("Enter a number: ");
     scanf("%d", &num);
-    check_prime(num);
+
+    if (is_prime(num))
+        printf("%d is a prime number.\n", num);
+    else
+        printf("%d is not a prime number.\n", num);
+
     return 0;
 }
