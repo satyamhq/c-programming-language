326a508376778701ba73e17ed779be41cd08efe3
Satyam Kumar <satyam31sk@gmail.com>
2026-03-10 15:11:06 +0530

    create 01-recursion.c

---
+++ module-5-recursion/01-recursion.c
@@ -0,0 +1,11 @@
+/* Recursion in C
+   Recursion is when a function calls itself to solve a smaller
+   version of the same problem, until it reaches a base case.
+
+   Basic Structure:
+
+   return_type function(params) {
+       if (base_case) return result;      // STOP condition
+       return function(smaller_problem);  // Recursive call
+   }
+*/
