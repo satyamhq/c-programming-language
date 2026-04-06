5d0eaeec676f2750f5b22cd4300ecffa5875a419
Satyam Kumar <satyam31sk@gmail.com>
2026-04-06 14:49:56 +0530

    create 14-matrix-addition-2.c

---
+++ module-7-array/14-matrix-addition-2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int A[3][3], B[3][3], C[3][3];
+
+    // Input matrix A
+    printf("Enter elements of matrix A (3x3):\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Input matrix B
+    printf("Enter elements of matrix B (3x3):\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Matrix addition
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    // Output result
+    printf("Sum of the matrices:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
