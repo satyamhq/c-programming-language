cdea9f0175b3f9e186cc14c20b29a89472dc31e9
Satyam Kumar <satyam31sk@gmail.com>
2026-04-06 15:04:21 +0530

    update 15-matrix-multiplication.c

---
+++ module-7-array/15-matrix-multiplication.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main() {
+    int A[3][3], B[3][3], C[3][3];
+
+    // Input matrix A
+    printf("Enter elements of matrix A (3x3):\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Input matrix B
+    printf("Enter elements of matrix B (3x3):\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Matrix multiplication
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < 3; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    // Output result
+    printf("Product of the matrices:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
