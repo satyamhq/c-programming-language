b158d36646b556e43a76d9e27bd3b4c1e4677109
Satyam Kumar <satyam31sk@gmail.com>
2026-04-07 14:53:38 +0530

    update 17-bubble-sort.c

--- module-7-array/17-bubble-sort.c
+++ module-7-array/17-bubble-sort.c
@@ -1,37 +1,30 @@
-// Bubble Sort
 #include <stdio.h>
 
-void bubbleSort(int arr[], int n) {
-    int i, j, temp;
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap elements
+int main() {
+    int arr[] = {5, 3, 2, 4};
+    int n = 4;
+    int i, j, temp, swapped;
+
+    for(i = 0; i < n-1; i++) {
+        swapped = 0;
+
+        for(j = 0; j < n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
                 temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swapped = 1;
             }
         }
+
+        if(swapped == 0) {
+            break; // already sorted
+        }
     }
-}
 
-int main() {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int i;
-
-    printf("Original array: ");
-    for (i = 0; i < n; i++) {
+    for(i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
-
-    bubbleSort(arr, n);
-
-    printf("\nSorted array: ");
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
 
     return 0;
 }
