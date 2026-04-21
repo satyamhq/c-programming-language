15594ef0144af4fb44241cc660afaa9647412397
Satyam Kumar <satyam31sk@gmail.com>
2026-04-21 14:50:36 +0530

    update 04-pointer-to-array-2.c

--- module-6-pointers/04-pointer-to-array-2.c
+++ module-6-pointers/04-pointer-to-array-2.c
@@ -1,19 +1,28 @@
 #include<stdio.h>
 
 int main() {
-    int n = 5;
     int arr[5] = {12, 55, 88, 125, 178};
 
-    printf("%p", arr);   // prints base address (address of arr[0])
+    printf("Base address:\n");
+    printf("%p\n", arr);
 
-    printf("\n%p", &arr[0]); // also prints address of arr[0]
-    printf("\n%p", &arr[1]); // prints address of arr[1]
-    printf("\n%p", &arr[2]); // prints address of arr[2]   // printf("%p", arr + 2); --> same as &arr[2]
-    printf("\n%p", &arr[3]); // prints address of arr[3]
-    printf("\n%p", &arr[4]); // prints address of arr[4]
+    printf("\nAddresses of elements:\n");
+    printf("arr[0]: %p\n", &arr[0]);
+    printf("arr[1]: %p\n", &arr[1]);
+    printf("arr[2]: %p\n", &arr[2]);
+    printf("arr[3]: %p\n", &arr[3]);
+    printf("arr[4]: %p\n", &arr[4]);
 
-    int *p = arr; // p points to arr[0]
-    printf("\n%p", p); // prints address of arr[0]
+    int *p = arr;
+
+    printf("\nPointer operations:\n");
+
+    printf("p (start): %p\n", p);
+
+    printf("p++ : %p\n", p++);   // prints arr[0], then moves to arr[1]
+    printf("++p : %p\n", ++p);   // moves to arr[2], then prints
+    printf("p-- : %p\n", p--);   // prints arr[2], then moves to arr[1]
+    printf("--p : %p\n", --p);   // moves to arr[0], then prints
 
     return 0;
 }
