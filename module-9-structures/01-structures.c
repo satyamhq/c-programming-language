618c7db4fee6dbfd911e0ba3dc7dc66c84a57094
Satyam Kumar <satyam31sk@gmail.com>
2026-05-04 15:46:46 +0530

    create 01-structures.c

---
+++ module-9-structures/01-structures.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+struct car{
+    char *name;
+    int seats;
+    float price;
+}; // end of structure
+
+int main(){
+    struct car mycar; // declare a variable of type struct car
+    mycar.name = "BMW";
+    mycar.seats = 5;
+    mycar.price = 50000.0;
+    printf("%s %d %f", mycar.name, mycar.seats, mycar.price);
+    return 0;
+}   
